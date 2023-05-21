c31cae44364a083dcf04e42ee6022912b31ebe56
Alexander Chapchuk <BigTows@yandex.ru>
2023-05-21 16:03:21 +0300

    Swagger docs.
    https://github.com/Hackathon-X-MODE/order-service/issues/4

--- src/main/java/com/example/example/controller/OrderImportController.java
+++ src/main/java/com/example/example/controller/OrderImportController.java
@@ -2,6 +2,7 @@ package com.example.example.controller;
 
 import com.example.example.config.WebConstants;
 import com.example.example.service.importer.ExcelImportService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -18,6 +19,10 @@ public class OrderImportController {
 
     private final ExcelImportService excelImportService;
 
+    @Operation(summary = "Загрузка комментариев через EXCEL-файл",
+            description = "Необходим xlsx файл, с примерно такой структурой" +
+                    " Комментарий|Дата|Оценка|Сумма заказа|Номер заказа (может отсутствовать)\n" +
+                    "По системе будет разлиты следующие данные: Комментарий, Оценка, Номер заказа (будет сгенерирован, если отсутствовал)")
     @PostMapping(value = "/xlsx", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     @ResponseStatus(HttpStatus.ACCEPTED)
     public void importViaExcel(@RequestHeader(WebConstants.Header.EXTERNAL_SYSTEM) String vendorCode,
