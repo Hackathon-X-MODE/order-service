d1e2e00920bbb2a7ff8c1e916a74893725aa3834
Alexander Chapchuk <BigTows@yandex.ru>
2023-05-24 16:07:59 +0300

    Update.

--- src/main/java/com/example/example/domain/DateHistory.java
+++ src/main/java/com/example/example/domain/DateHistory.java
@@ -1,6 +1,5 @@
 package com.example.example.domain;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import io.hypersistence.utils.hibernate.type.json.JsonType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
@@ -22,23 +21,23 @@ import java.util.List;
 public class DateHistory {
 
     @Column(name = "order_date_create")
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy hh:mm:ss")
+    //@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy hh:mm:ss")
     private LocalDateTime create;
 
     @Column(name = "order_date_assembling")
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy hh:mm:ss")
+    //@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy hh:mm:ss")
     private LocalDateTime assembling;
 
     @Column(name = "order_date_send")
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy hh:mm:ss")
+    //@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy hh:mm:ss")
     private LocalDateTime send;
 
     @Column(name = "order_date_receive")
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy hh:mm:ss")
+    //@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy hh:mm:ss")
     private LocalDateTime receive;
 
     @Column(name = "order_date_get")
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy hh:mm:ss")
+    //@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy hh:mm:ss")
     private LocalDateTime get;
 
     @Type(JsonType.class)
